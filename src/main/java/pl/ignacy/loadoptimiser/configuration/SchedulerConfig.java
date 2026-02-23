b35c99c6786fe2480284f194eacd5dc476e5bfe1
ignacybl <ignacybla@gmail.com>
2026-02-23 22:03:32 +0100

    redis

--- src/main/java/pl/ignacy/loadoptimiser/configuration/SchedulerConfig.java
+++ src/main/java/pl/ignacy/loadoptimiser/configuration/SchedulerConfig.java
@@ -1,21 +1,20 @@
 package pl.ignacy.loadoptimiser.configuration;
 
 import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
 public class SchedulerConfig {
     @Bean
-    public LockProvider lockProvider(DataSource dataSource){
-        return new JdbcTemplateLockProvider(JdbcTemplateLockProvider.Configuration.builder().withJdbcTemplate(new JdbcTemplate(dataSource)).usingDbTime().build());
+    public LockProvider lockProvider(RedisConnectionFactory connectionFactory){
+        return new RedisLockProvider(connectionFactory, "load-optimiser-locks");
+
     }
 }
